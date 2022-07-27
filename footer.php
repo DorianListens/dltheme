8020c96b4c9299d9c8d753a7fc87209f3326849f
Dorian Scheidt <dorian.scheidt@gmail.com>
2022-07-26 23:04:45 -0500

    remove phone and change email in footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         		<p class="footer-text" >&copy; <?php echo date('Y'); ?> Dorian Scheidt</p> <!-- class="source-org copyright" -->
         	</div> <!-- end #inner-footer -->
         	<div class="footer-iconlinks">
-<a href="mailto:BigD@dorianlistens.com" target="_blank"><i class="icon-mail"></i></a><a href="tel:+15146282927" target="_blank"><i class="icon-phone"></i></a><a href="https://twitter.com/DorianListens" target="_blank"><i class="icon-twitter"></i></a><a href="http://github.com/DorianListens" target="_blank"><i class="icon-github"></i></a><a href="https://linkedin.com/in/DorianListens" target="_blank"><i class="icon-linkedin"></i></a></div>
+<a href="mailto:dorian.scheidt@gmail.com" target="_blank"><i class="icon-mail"></i></a><a href="https://twitter.com/DorianListens" target="_blank"><i class="icon-twitter"></i></a><a href="http://github.com/DorianListens" target="_blank"><i class="icon-github"></i></a><a href="https://linkedin.com/in/DorianListens" target="_blank"><i class="icon-linkedin"></i></a></div>
       	</footer> <!-- end footer -->
     </div>
 </div>
